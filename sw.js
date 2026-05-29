a95e137080964fbf7101389db95e28de2e20e383
AngelaBDS <fumouse88@gmail.com>
2026-05-29 11:44:28 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'bd-career-matrix-cache-v1';
 const urlsToCache = [
   'index.html',
   'manifest.json',
-  'bd_app_icon.png'
+  'bd_app_icon_1.jpeg'
 ];
 
 self.addEventListener('install', event => {
